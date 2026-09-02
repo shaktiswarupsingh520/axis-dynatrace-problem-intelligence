4978a39496c4365f8b73ed42ce2ad5778eb36434
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-09-02 18:40:54 +0530

    feat: expose alert dump and Davis RCA navigation

--- ui/app/components/Header.tsx
+++ ui/app/components/Header.tsx
@@ -6,5 +6,7 @@ export const Header = () => <AppHeader>
   <AppHeader.Navigation>
     <AppHeader.Logo as={Link} to="/" />
     <AppHeader.NavigationItem as={Link} to="/">Problem Intelligence</AppHeader.NavigationItem>
+    <AppHeader.NavigationItem as={Link} to="/alert-dump">Alert Dump</AppHeader.NavigationItem>
+    <AppHeader.NavigationItem as={Link} to="/rca">RCA with Davis</AppHeader.NavigationItem>
   </AppHeader.Navigation>
 </AppHeader>;
