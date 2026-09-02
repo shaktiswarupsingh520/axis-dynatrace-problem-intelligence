b34dc34fc170f6c033e0092ab8c8c86e8960a0c1
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-09-02 18:11:06 +0530

    refactor: make problem intelligence the only app route

--- ui/app/App.tsx
+++ ui/app/App.tsx
@@ -1,22 +1,10 @@
-import { PageLayout } from "@dynatrace/strato-components/layouts";
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Data } from "./pages/Data";
-import { Header } from "./components/Header";
-import { Home } from "./pages/Home";
+import { PageLayout } from '@dynatrace/strato-components/layouts';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
 
-export const App = () => {
-  return (
-    <PageLayout>
-      <PageLayout.Header>
-        <Header />
-      </PageLayout.Header>
-      <PageLayout.Content>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/data" element={<Data />} />
-        </Routes>
-      </PageLayout.Content>
-    </PageLayout>
-  );
-};
+export const App = () => <PageLayout>
+  <PageLayout.Header><Header /></PageLayout.Header>
+  <PageLayout.Content><Routes><Route path="/" element={<Home />} /></Routes></PageLayout.Content>
+</PageLayout>;
