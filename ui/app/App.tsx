0a0389b9d1b31051abc24eb85b6c10fdac317927
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-09-02 18:40:49 +0530

    feat: add alert dump and RCA routes

--- ui/app/App.tsx
+++ ui/app/App.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import { AlertDump } from './pages/AlertDump';
+import { RcaWorkbench } from './pages/RcaWorkbench';
 
 export const App = () => <PageLayout>
   <PageLayout.Header><Header /></PageLayout.Header>
-  <PageLayout.Content><Routes><Route path="/" element={<Home />} /></Routes></PageLayout.Content>
+  <PageLayout.Content><Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/alert-dump" element={<AlertDump />} />
+    <Route path="/rca" element={<RcaWorkbench />} />
+  </Routes></PageLayout.Content>
 </PageLayout>;
